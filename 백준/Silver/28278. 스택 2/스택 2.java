a76b6359c2191f349e1048e713c529a629a3bcbd
YONGYONGB <aa39421371@gmail.com>
2025-07-13 18:02:56 +0900

    [Silver IV] Title: 스택 2, Time: 912 ms, Memory: 235576 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -6,52 +6,30 @@ public class Main{
     public static void main(String[] args)throws IOException{
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int n = Integer.parseInt(br.readLine());
-        
+        StringBuilder sb = new StringBuilder();
         Stack<Integer> st = new Stack<>();
-        
-        for(int i=0; i<n; i++){
-            String[] input = br.readLine().split(" ");
-            int num = Integer.parseInt(input[0]);
-            int in = 0;
-            if (input.length > 1) {
-                in = Integer.parseInt(input[1]);
-            }
-            input(st, num, in);
-        }
-        
-        
-    }
-    
-    public static Stack<Integer> input(Stack<Integer> st, int num, int in){
-        
-        if(num == 1){
-            st.push(in);
-        }else if(num ==2){
-            if(!st.isEmpty()){
-                int temp = st.pop();
-                System.out.println(temp);
-            }else if(st.isEmpty()){
-                System.out.println(-1);
-            }
-        }else if(num ==3){
-            System.out.println(st.size());
-        }else if(num ==4){
-            if(!st.isEmpty()){
-                System.out.println(0);
-            }else if(st.isEmpty()){
-                System.out.println(1);
-            }
-        }else if(num ==5){
-            if(!st.isEmpty()){
-                System.out.println(st.peek());
-            }else if(st.isEmpty()){
-                System.out.println(-1);
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer stz = new StringTokenizer(br.readLine());
+            int cmd = Integer.parseInt(stz.nextToken());
+
+            if (cmd == 1) {
+                st.push(Integer.parseInt(stz.nextToken()));
+            } else if (cmd == 2) {
+                sb.append(st.isEmpty() ? -1 : st.pop()).append('\n');
+            } else if (cmd == 3) {
+                sb.append(st.size()).append('\n');
+            } else if (cmd == 4) {
+                sb.append(st.isEmpty() ? 1 : 0).append('\n');
+            } else if (cmd == 5) {
+                sb.append(st.isEmpty() ? -1 : st.peek()).append('\n');
             }
         }
+        System.out.print(sb);
         
-        return st;
+        
     }
     
 }
