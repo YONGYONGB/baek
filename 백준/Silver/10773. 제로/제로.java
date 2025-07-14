87d2e0ae3128bd1c8f8dea7371ce17ec08e323b1
YONGYONGB <aa39421371@gmail.com>
2025-07-14 10:00:10 +0900

    [Silver IV] Title: 제로, Time: 216 ms, Memory: 23956 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    
+    public static void main(String[] args)throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n =Integer.parseInt(br.readLine());
+        int ans = 0;
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i=0; i<n; i++){
+            int num = Integer.parseInt(br.readLine());
+            if(num == 0 ){
+                stack.pop();
+            }else{
+                stack.push(num);
+            }
+        }
+        
+        while(stack.size() != 0){
+            ans += stack.pop();
+        }
+        
+        System.out.print(ans);
+        
+    }
+    
+}
