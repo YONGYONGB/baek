3efde391713e19a067c51a27d04aef8e90f388ea
YONGYONGB <aa39421371@gmail.com>
2025-07-15 10:08:07 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 116 ms, Memory: 14420 KB -BaekjoonHub

---
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        boolean getFood = true;
+        String[] row = br.readLine().split(" ");
+        
+        int count = 1;
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i=0; i<n; i++){
+            int num = Integer.parseInt(row[i]);
+            if(num != count){
+                stack.add(num);
+            }else{
+                count++;
+                while(true){
+                    if(!stack.isEmpty() && stack.peek() == count){
+                        stack.pop();
+                        count++;
+                    }else{
+                        break;
+                    }
+                }
+            }
+        }
+        
+        if(stack.size() != 0 || count != n+1){
+            getFood = false;
+        }
+        
+        
+       
+        if(getFood == true){
+            sb.append("Nice");
+        }else{
+            sb.append("Sad");
+        }
+        
+        System.out.print(sb);
+        
+    }
+}
