45b26a0e31c9d4c0aff8f06985b59712d4a04a5e
YONGYONGB <aa39421371@gmail.com>
2025-07-31 10:39:28 +0900

    [level 1] Title: 모의고사, Time: 14.83 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,30 +2,30 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
         List<Integer> answer = new ArrayList<>();
-        int[] a1 = {1,2,3,4,5};
-        int[] a2 = {2,1,2,3,2,4,2,5};
-        int[] a3 = {3,3,1,1,2,2,4,4,5,5};
+        int[] one = {1,2,3,4,5};
+        int[] two = {2,1,2,3,2,4,2,5};
+        int[] three = {3,3,1,1,2,2,4,4,5,5};
         
-        int num1 = ans(answers,a1);
-        int num2 = ans(answers,a2);
-        int num3 = ans(answers,a3);
         
-        int max = Math.max(num1, Math.max(num2,num3));
-        if(max == num1) answer.add(1);
-        if(max == num2) answer.add(2);
-        if(max == num3) answer.add(3);
+        int ans_one = check(answers,one,0);
+        int ans_two = check(answers,two,0);
+        int ans_three = check(answers,three,0);
+        
+        int max = (int)Math.max(ans_one, Math.max(ans_two,ans_three));
+        if(max == ans_one) answer.add(1);
+        if(max == ans_two) answer.add(2);
+        if(max == ans_three) answer.add(3);
         
         return answer.stream().mapToInt(i -> i).toArray();
     }
-    public int ans(int[] answers, int[] a){
-        int number =0;
-        for(int i=0; i<answers.length; i++){
-            if(answers[i] == a[i%a.length]){
-                number++;
-            }
+    public int check(int[] answers, int[] hew, int index){
+        if(index == answers.length){
+            return 0;
         }
-        
-        return number;
+        int score = 0;
+        if(answers[index]==hew[index%hew.length]){
+            score = 1;
+        }
+        return score + check(answers,hew,index+1);
     }
-    
 }
